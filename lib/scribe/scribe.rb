3df6b22c7a92a696b703aa092c4967d0d7134b8b
Evan Weaver <eweaver@twitter.com>
2009-10-13 21:38:44 -0700

    Pass through settings.

--- lib/scribe/scribe.rb
+++ lib/scribe/scribe.rb
@@ -1,12 +1,13 @@
 
 class Scribe
   # Created a new client instance. Accepts an optional host, port, default 
-  # category, and flag to control whether newlines are added to each line.
-  def initialize(servers = "127.0.0.1:1463", category = "Ruby", add_newlines = true)
+  # category, flag to control whether newlines are added to each line, and
+  # additional settings to be passed to ThriftClient.
+  def initialize(servers = "127.0.0.1:1463", category = "Ruby", add_newlines = true, thrift_client_options = {})
     @servers = servers
     @category = category
     @add_newlines = add_newlines
-    @client = ThriftClient.new(ScribeThrift::Client, @servers, :randomize_server_list => true)
+    @client = ThriftClient.new(ScribeThrift::Client, @servers, thrift_client_options)
   end
 
   # Log a message. Accepts a string and an optional category.
